341af0d369fd33468283962f4b4b62123a25fe33
Olga Smirnova <olgasmirnova@Olgas-MBP.localdomain>
2015-09-01 21:02:43 +1200

    gulp set up

---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<!--[if lt IE 7 ]><html class="ie ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]><html class="ie ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]><html class="ie ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--><html lang="en" class="no-js"> <!--<![endif]-->
+<head>
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<meta name="descripion" content="Simple JS Form Validation"> <!-- recomended 160 chrs, will be shown on search engine result pages -->
+	<meta name="robots" content="noindex, nofollow" />
+	<meta name="keywords" content="???"/>
+	<meta charset="utf-8" />
+
+	<title>Template 5</title>
+	
+<!-- Bootstrap -->   
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+<!-- Fonts -->	
+	<link href='http://fonts.googleapis.com/css?family=Roboto:400,300,100' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab:400,700' rel='stylesheet' type='text/css'>
+
+	<link href="dist/assets/css/styles.min.css" rel="stylesheet" type="text/css">
+
+</head>
+
+
+<body>
+<!--================== Backups =================================================-->
+<!--[if lte IE 9]>
+	<div class="message" id="oldie">
+		<p>Sorry, but this website does not support IE9 or lower. Please <a href="http://windows.microsoft.com/ie">upgrade your IE</a> or <a href="http://www.browsehappy.com">switch to another browser</a>.</p>
+	</div>
+<![endif]-->
+
+<!-- Opera Mini backup -->
+	<div style="display:none;" class="message" id="operamini">
+		<p>Sorry, but this website does not support Opera Mini. Please switch to another browser such as Opera for Android, Chrome, Firefox, or Safari.</p>
+	</div>
+
+<!-- no JS backup -->
+	<noscript>
+	    <div class="message" id="nojs">
+	    	<p class="text-center"><i class="fa fa-exclamation-circle"></i> Sorry, but you need JavaScript to enjoy full experience from this page. Please, enable JavaScript in your browser or switch to a JavaScript device.</p>
+	    </div>
+	</noscript>
+
+
+
+<!--================== Header =================================================-->
+	<header>
+		header
+
+    </header> <!-- header ends -->
+
+
+
+<!--================== Main =================================================-->
+		<div class="container-fluid">
+
+			<main class="row">
+			
+				main
+			</main>
+
+		</div>
+
+
+
+<!--================== Footer =================================================-->
+		<footer role="contentinfo">
+			
+		</footer>
+
+
+		
+	
+<!-- jQuery -->	
+	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	
+	<script type="text/javascript" src="src/scripts/formValidation.js"></script>
+	<script type="text/javascript" src="dist/assets/js/main.js"></script>
+
+</body>
+
+</html>
